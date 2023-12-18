71f89a041fa8673afc7054006843a66084477d2e
Onur Esin <onuresn034@gmail.com>
2023-12-18 13:41:12 +0300

    konu seçim bölümü tamam

---
+++ src/Topicselection.jsx
@@ -0,0 +1,16 @@
+export default function Topicselection() {
+    <div className="select-topic">
+        <button>
+            <img src="/public/assets/img/HTML-logo.svg" alt="HTML Icon" /><h5>HTML</h5>
+        </button>
+        <button>
+            <img src="/public/assets/img/CSS-logo.svg" alt="CSS Icon" /><h5>CSS</h5>
+        </button>
+        <button>
+            <img src="/public/assets/img/JS-logo.svg" alt="JS Icon" /><h5>JS Icon</h5>
+        </button>
+        <button>
+            <img src="/public/assets/img/Accessebility-logo.svg" alt="Accessebility Icon" /> <h5>Accessebilitiy</h5>
+        </button>
+    </div>
+}
