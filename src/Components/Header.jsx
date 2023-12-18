51aa69d846548cc24a207c241f6ec72fb7a6f493
Onur Esin <onuresn034@gmail.com>
2023-12-18 16:57:09 +0300

    bir kaç değişiklik

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
     return (
         <div className="switch-division">
-        <img src="/public/assets/img/light-mode-button.svg" alt="light switch icon" />
+        <img src="images/light-mode-button.svg" alt="light switch icon" />
             <div onClick={() => document.body.classList.toogle("dark-theme")} className="switchSide">
-                <img src="/public/assets/images/circle-switch.svg"/>
+                <img src="/images/circle-switch.svg"/>
             </div>
-        <img src="/public/assets/img/dark-mode-button.svg" alt="dark switch icon"/>
+        <img src="images/dark-mode-button.svg" alt="dark switch icon"/>
     </div>
     )
 }
---
+++ src/Components/Topicselection.jsx
@@ -0,0 +1,16 @@
+export default function Topicselection() {
+    <div className="select-topic">
+        <button>
+            <img src="images/HTML-logo.svg" alt="HTML Icon" /><h5>HTML</h5>
+        </button>
+        <button>
+            <img src="images/CSS-logo.svg" alt="CSS Icon" /><h5>CSS</h5>
+        </button>
+        <button>
+            <img src="images/JS-logo.svg" alt="JS Icon" /><h5>JS Icon</h5>
+        </button>
+        <button>
+            <img src="images/Accessebility-logo.svg" alt="Accessebility Icon" /> <h5>Accessebilitiy</h5>
+        </button>
+    </div>
+}
