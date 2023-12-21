25fb126e68d64fe5eaf273f3e55f26851e98bcb8
Onur Esin <onuresn034@gmail.com>
2023-12-21 21:15:04 +0300

    switch buton eklendi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [light, setLight] = useState(true);
+
     return (
         <div className="switch-division">
-        <img src="images/light-mode-button.svg" alt="light switch icon" />
-            <div onClick={() => document.body.classList.toggle("dark-theme")} className="switchSide">
-                <img src="/images/circle-switch.svg"/>
-            </div>
-        <img src="images/dark-mode-button.svg" alt="dark switch icon"/>
-    </div>
-    )
-}
+          <img src="images/light-mode-button.svg" alt="light switch icon" />
+          <div onClick={() => document.body.classList.toggle("dark-theme")} className="switchSide">
+            <label style={{ visibility: !light ? "hidden" : "visible" }} onClick={() => setLight(false)}>
+              <input type="radio" name="dark" checked={light} onChange={() => setLight(false)} />
+            </label>
+            <label style={{ visibility: light ? "hidden" : "visible" }} onClick={() => setLight(true)}>
+              <input type="radio" name="light" defaultChecked={!light} onChange={() => setLight(true)} />
+            </label>
+          </div>
+          <img src="images/dark-mode-button.svg" alt="dark switch icon" />
+        </div>
+      );
+    }
