a2c9e0ef5b9dd398e37acde6828d2f41368a8407
Onur Esin <onuresn034@gmail.com>
2024-01-02 12:19:22 +0300

    dark mode hataları %80 düzeltildi

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 
 export default function Header() {
-    const [light, setLight] = useState(true);
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const handleThemeChange = () => {
+    setDarkTheme(!darkTheme);
+  }
+  useEffect(() => {
+    document.body.classList.toggle('dark-theme', darkTheme);
+  }, [darkTheme]);
 
     return (
         <div className="switch-division">
-          <img src="images/light-mode-button.svg" alt="light switch icon" />
-          <div onClick={() => document.body.classList.toggle("dark-theme")} className="switchSide">
-            <label style={{ visibility: !light ? "hidden" : "visible" }} onClick={() => setLight(false)}>
-              <input type="radio" name="dark" checked={light} onChange={() => setLight(false)} />
-            </label>
-            <label style={{ visibility: light ? "hidden" : "visible" }} onClick={() => setLight(true)}>
-              <input type="radio" name="light" defaultChecked={!light} onChange={() => setLight(true)} />
-            </label>
-          </div>
-          <img src="images/dark-mode-button.svg" alt="dark switch icon" />
+          <span className="right-theme" onClick={handleThemeChange}>
+              {darkTheme ? 
+                (<img src="images/darkmode.svg" alt="light mode" />) : 
+                (<img src="images/lightmode.svg" alt="dark mode" />)}
+          </span>
         </div>
       );
     }
